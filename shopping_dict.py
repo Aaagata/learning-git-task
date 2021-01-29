6e1a0b44cc5ab53122ec9ff5c7cec9ae8dc19d7f
Agata Dunaj <a.dunaj@yahoo.co.uk>
2021-01-29 14:13:47 +0100

    Add shopping list

--- shopping_dict.py
+++ shopping_dict.py
@@ -2,3 +2,6 @@ shopping_dict = {
     "warzywniak": ["marchew", "rukola", "seler"],
     "piekarnia": ["pączek", "bułki", "chleb"]
 }
+for k, v in shopping_dict.items():
+    print(f"Idę do {k} i kupię tam {v}")
+    
