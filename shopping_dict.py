6087b1a8c80373c2be56be5425107eac93db19e6
Agata Dunaj <a.dunaj@yahoo.co.uk>
2021-01-29 14:17:15 +0100

    Add sum of all items

--- shopping_dict.py
+++ shopping_dict.py
@@ -4,4 +4,7 @@ shopping_dict = {
 }
 for k, v in shopping_dict.items():
     print(f"Idę do {k} i kupię tam {v}")
+list = shopping_dict.get("warzywniak") + shopping_dict.get("piekarnia")
+no_of_items = len(list)
+print(f'W sumie kupię {no_of_items} produktów.')
     
