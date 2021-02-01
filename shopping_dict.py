67eb65103eafbcdf5762be91ac3ca13a341ef92a
Agata Dunaj <a.dunaj@yahoo.co.uk>
2021-02-01 17:40:02 +0100

    fix lista

--- shopping_dict.py
+++ shopping_dict.py
@@ -1,11 +1,12 @@
 shopping_dict = {
     "warzywniak": ["marchew", "rukola", "seler"],
-    "piekarnia": ["pączek", "bułki", "chleb"]
+    "piekarnia": ["pączek", "bułki", "chleb"],
+    "ikea": ["hot-dog", "wieszaki","bambus"],
 }
+no_of_items = 0
 for k, v in shopping_dict.items():
+    no_of_items += len(v)
     v_c = [item.capitalize() for item in v]
     print(f"Idę do {k.capitalize()} i kupię tam {v_c}")
-list = shopping_dict.get("warzywniak") + shopping_dict.get("piekarnia")
-no_of_items = len(list)
+
 print(f'W sumie kupię {no_of_items} produktów.')
-print("I to by było na tyle")
