42d5ad09db3226a09b3d0a54c5ec9044d3bccf8c
Aaagata <78355520+Aaagata@users.noreply.github.com>
2021-02-01 17:24:42 +0100

    change letter to capital

--- shopping_dict.py
+++ shopping_dict.py
@@ -3,7 +3,8 @@ shopping_dict = {
     "piekarnia": ["pączek", "bułki", "chleb"]
 }
 for k, v in shopping_dict.items():
-    print(f"Idę do {k.capitalize()} i kupię tam {v}")
+    v_c = [item.capitalize() for item in v]
+    print(f"Idę do {k.capitalize()} i kupię tam {v_c}")
 list = shopping_dict.get("warzywniak") + shopping_dict.get("piekarnia")
 no_of_items = len(list)
 print(f'W sumie kupię {no_of_items} produktów.')
